34c1e471aa1a78c0807a07c6296babc29325daa6
dhvanilrathod <1151.dhvanilrathod@gmail.com>
2024-02-06 13:04:33 +0530

    All code Up-to-date

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, {useEffect} from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigators from './src/navigators/TabNavigators';
+import DetailsScreen from './src/screens/DetailsScreen'
+import PaymentScreen from './src/screens/PaymentScreen'
+import SplashScreen from 'react-native-splash-screen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  },[])
+  return (
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen  name="Tab" component={TabNavigators} options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+      <Stack.Screen  name="Details" component={DetailsScreen} options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+      <Stack.Screen  name="Payment" component={PaymentScreen} options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
